3f389088462e9f86fa5a246e80a31f717344bddf
87oui <hello@87oui.com>
2023-04-05 16:01:56 +0900

    page.php テンプレートを読み込むロジックを修正

--- page.php
+++ page.php
@@ -28,17 +28,11 @@ $ancestors = array_map(
 	},
 	array_reverse( get_post_ancestors( $timber_post->ID ) )
 );
+$page_theme = implode( '-', array( 'page', ...$ancestors, $timber_post->post_name ) ) . '.twig';
 $ancestor_themes = array();
 while ( ! empty( $ancestors ) ) {
 	array_push( $ancestor_themes, implode( '-', array( 'page', ...$ancestors ) ) . '.twig' );
 	array_pop( $ancestors );
 }
 
-Timber::render(
-	array(
-		implode( '-', array( 'page', ...$ancestors, $timber_post->post_name ) ) . '.twig',
-		...$ancestor_themes,
-		'page.twig',
-	),
-	$context
-);
+Timber::render( array( $page_theme, ...$ancestor_themes, 'page.twig' ), $context );
