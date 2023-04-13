9f80d945112e8d5adc81051980db5619b472284a
87oui <59995553+87oui@users.noreply.github.com>
2023-04-13 17:06:35 +0900

    phpcsとprettierを依存関係に追加

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  plugins: [require('prettier-plugin-twig-melody')],
+  twigPrintWidth: 120,
+};
