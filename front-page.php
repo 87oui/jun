6d6f1c388a6c92f52c346f01f1d228752d0a8fcc
87oui <hello@87oui.com>
2023-12-12 15:31:52 +0900

    ホームページに固定ページを指定できるように

--- front-page.php
+++ front-page.php
@@ -8,5 +8,8 @@
 $context = Timber::context();
 $context['title'] = apply_filters( 'jun_page_title', '' );
 $context['posts'] = new Timber\PostQuery();
+if ( 'page' === get_option( 'show_on_front' ) ) {
+	$context['post'] = new Timber\Post();
+}
 $templates = array( 'front-page.twig', 'index.twig' );
 Timber::render( $templates, $context );
