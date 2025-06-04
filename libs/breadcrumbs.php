1224251c6c8c884d8d2c76eb996dd9122863670e
87oui <hello@87oui.com>
2025-06-04 13:36:47 +0900

    POST_TYPE_DEFAUKT_SLUG,POST_TYPE_DEFAULT_LABEL を削除

--- libs/breadcrumbs.php
+++ libs/breadcrumbs.php
@@ -17,23 +17,6 @@ add_filter(
 			return array();
 		}
 
-		// 投稿ポストタイプにスラッグが設定されている場合、パンくずリストにアーカイブページを挿入
-		if ( defined( 'POST_TYPE_DEFAULT_LABEL' )
-			&& ( is_post_type_archive( 'post' ) || is_singular( 'post' ) )
-		) {
-			array_splice(
-				$items,
-				1,
-				0,
-				array(
-					array(
-						'title' => POST_TYPE_DEFAULT_LABEL,
-						'link' => get_post_type_archive_link( 'post' ),
-					),
-				)
-			);
-		}
-
 		// 最後の項目のリンクを外す
 		unset( $items[ count( $items ) - 1 ]['link'] );
 
