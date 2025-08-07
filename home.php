20cb07b403bad12301b5dcda26e37ca3d45432cb
87oui <hello@87oui.com>
2025-08-07 11:20:13 +0900

    トップページの不要な処理を削除、front_page.phpでの投稿取得を削除したため必要に応じて子テーマで取得するように

--- home.php
+++ home.php
@@ -8,6 +8,5 @@
 $context = Timber::context();
 $post_type_obj = get_post_type_object( get_post_type() );
 $context['title'] = apply_filters( 'jun_page_title', $post_type_obj->label, array( 'post_type' => $post_type_obj ) );
-$context['posts'] = Timber::get_posts();
 $templates        = array( 'home.twig', 'archive.twig', 'index.twig' );
 Timber::render( $templates, $context );
